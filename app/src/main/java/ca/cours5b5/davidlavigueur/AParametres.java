1fcda7c1fa56a5daab014311d88564b442298031
David Lavigueur <davidlavigueur@live.ca>
2019-08-26 13:28:20 -0500

    Ajout de l'activité vide de base. Celle-ci s'ouvre sur l'émulateur

---
+++ app/src/main/java/ca/cours5b5/davidlavigueur/AParametres.java
@@ -0,0 +1,14 @@
+package ca.cours5b5.davidlavigueur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AParametres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_parametres);
+    }
+}
